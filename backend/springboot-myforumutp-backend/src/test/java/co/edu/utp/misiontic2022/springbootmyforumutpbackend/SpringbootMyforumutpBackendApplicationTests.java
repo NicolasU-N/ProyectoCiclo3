1359a5757cee0d517898d55fa06e126f596413ad
Nicolas Useche <nico311021@gmail.com>
2022-09-11 01:49:42 -0500

    Add project

---
+++ backend/springboot-myforumutp-backend/src/test/java/co/edu/utp/misiontic2022/springbootmyforumutpbackend/SpringbootMyforumutpBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package co.edu.utp.misiontic2022.springbootmyforumutpbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootMyforumutpBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
