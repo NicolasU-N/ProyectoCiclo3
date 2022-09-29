b5de480b3e4c1ed50e1ab374e9156a4cf016f813
aeromeror <aeromeror@unal.edu.co>
2022-09-28 22:20:32 -0500

    avanze vistas

--- frontend/myforumutp-front/src/main.js
+++ frontend/myforumutp-front/src/main.js
@@ -1,6 +1,7 @@
 import { createApp } from 'vue'
 import App from './App.vue'
-
+import router from './router/router.js'
 import './assets/main.css'
+import VueSweetalert2 from 'vue-sweetalert2';
 
-createApp(App).mount('#app')
+createApp(App).use(router).use(VueSweetalert2).mount('#app')
---
+++ frontend/myforumutp-front/src/views/Home.vue
@@ -0,0 +1,26 @@
+<template>
+    <div class="home">
+      
+      <h1>Home</h1>
+      
+    </div>
+  </template>
+  
+  <script>
+  import Navigation from "../components/Navigation.vue";
+  //import auth from "@/logic/auth";
+  export default {
+    name: "Home",
+    components: {
+      //navigation: Navigation
+    }/*,
+    computed: {
+      userLogged() {
+        return auth.getUserLogged();
+      }
+    }*/
+  };
+  </script>
+  
+  <style>
+  </style>
